960bdaf89e3e2e878e19214345db28ed5681846f
zer0vini <vini96mart@gmail.com>
2022-06-21 17:05:34 -0300

    Corrigindo

--- modem.py
+++ modem.py
@@ -3,8 +3,10 @@ import numpy as np
 
 class Modem:
     def __init__(self, fs, bufsz, ans=False):
+        self.bit_rate = 300
+        self.bits = []
         self.fs = fs  # taxa de amostragem
-        self.bufsz = bufsz  # quantidade de amostas que devem ser moduladas por vez
+        self.bufsz = bufsz  # quantidade de amostras que devem ser moduladas por vez
 
         # frequências de modulação (upload)
         self.tx_omega0 = 2*np.pi*(1080 + 100)
@@ -21,14 +23,37 @@ class Modem:
     # Modulação
 
     def put_bits(self, bits):
-        pass
+        if len(bits) == 0:
+            bits.append(1)
+        self.bits.extend(bits)
+
 
     def get_samples(self):
-        return np.zeros(self.bufsz)
+        x = np.arange(0, len(self.bits)/300, 1/self.fs)
+        y = []
+        t = 0.0
+        phi = 0
+
+        for i in self.bits:
+            w = (self.rx_omega0 if i == 0 else self.rx_omega1)
+            phi -= w*t
+            for j in range(self.fs/self.bit_rate):
+                x.append(t)
+                y.append(np.sin(t+phi))
+                t += 1/self.fs
+        self.bits = []
+        return np.array(y)
 
     # Demodulação
 
-    def put_samples(self, data):
+    #def put_samples(self, data):
+     #   for n in range (1, len(self.s)):
+      #      self.v0r[n] = self.s[n] - self.r**self.L * np.cos(self.rx_omega0*self.L*self.T)*self.s[n-self.L] + self.r*np.cos(self.rx_omega0*self.T)*self.v0r[n-1] - self.r*np.sin(self.rx_omega0*self.T) * self.v0i[n-1]
+       #     self.v0i[n] = -self.r**self.L * np.sin(self.rx_omega0*self.L*self.T)*self.s[n-self.L] + self.r*np.cos(self.rx_omega0*self.T)*self.v0i[n-1] - self.r*np.sin(self.rx_omega0*self.T) * self.v0r[n-1]
+
+        #    self.v1r[n] = self.s[n] - self.r**self.L * np.cos(self.rx_omega1*self.L*self.T)*self.s[n-self.L] + self.r*np.cos(self.rx_omega1*self.T)*self.v1r[n-1] - self.r*np.sin(self.rx_omega1*self.T) * self.v1i[n-1]
+         #   self.v1i[n] = -self.r**self.L * np.sin(self.rx_omega1*self.L*self.T)*self.s[n-self.L] + self.r*np.cos(self.rx_omega1*self.T)*self.v0i[n-1] - self.r*np.sin(self.rx_omega1*self.T) * self.v1r[n-1]
+        #rho = self.v1r**2+self.v1i**2+self.v0r**2+self.v0i**2
         pass
 
     def get_bits(self):
